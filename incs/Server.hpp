c3ff4523aa2d873ec3c2982b4a431c8a86e1ec7f
mamakaro <mamakaro@f1br4s5.paris.42.school>
2026-05-28 18:38:17 +0200

    Debut cmds Personne C

--- incs/Server.hpp
+++ incs/Server.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Server.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: edidier <edidier@student.42.fr>            +#+  +:+       +#+        */
+/*   By: mamakaro <mamakaro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/22 16:45:50 by edidier           #+#    #+#             */
-/*   Updated: 2026/05/28 16:57:10 by edidier          ###   ########.fr       */
+/*   Updated: 2026/05/28 18:03:45 by mamakaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@
 #include <string>
 #include <poll.h>
 #include "Client.hpp"
+#include "Channel.hpp"
+#include "parser_msg_irc.hpp"
 
 #define MAX_CLIENTS 64
 #define BUFFER_SIZE 1024
@@ -26,6 +28,7 @@ class Server {
         std::string                 _password;
         std::vector<struct pollfd>  _fds;
         std::vector<Client>         _clients;
+        Parser                      _parser;
 
         typedef void (Server::*CommandHandler)(Client&, std::vector<std::string>&);
         std::map<std::string, CommandHandler> _commands;
@@ -40,6 +43,20 @@ class Server {
         void cmdNick(Client& client, std::vector<std::string>& params);
         void cmdUser(Client& client, std::vector<std::string>& params);
         void cmdQuit(Client& client, std::vector<std::string>& params);
+        void cmdPing(Client& client, std::vector<std::string>& params);
+        void cmdPong(Client& client, std::vector<std::string>& params);
+        void cmdPrivmsg(Client& client, std::vector<std::string>& params);
+        void cmdNotice(Client& client, std::vector<std::string>& params);
+        void cmdMode(Client& client, std::vector<std::string>& params);
+
+        void sendToClient(int fd, const std::string& message);
+        Client* findClientByNick(const std::string& nick);
+        Channel* findChannel(const std::string& name);
+
+        std::vector<Channel> _channels;
+        std::map<std::string, Client*> _nickToClient;
+        std::map<std::string, Channel*> _nameToChannel;
+        
 
     public:
         Server(int port, const std::string& password);
